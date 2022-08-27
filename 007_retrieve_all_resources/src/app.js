2730ad040d987d35ace1607a734b0c2e36a6e6c3
leo_feola <feola.leopoldo@gmail.com>
2022-08-27 11:53:33 +0200

    07 bozza

--- 007_retrieve_all_resources/src/app.js
+++ 007_retrieve_all_resources/src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import "express-async-errors";
+import prisma from "./lib/prisma/client.js";
 
 const app = express();
 
 app.get("/", (request, response) => {
   response.json({ message: "Server Running" });
+});
+
+app.get("/cars", async (request, response) => {
+  const cars = await prisma.cars.findMany();
+  response.json(cars);
 });
 
 export default app;
